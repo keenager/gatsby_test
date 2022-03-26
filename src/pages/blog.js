fab19fba8cc51c94ebbc58c6c6ea3b9ee8706e8d
keenager <keenager84@gmail.com>
2022-03-27 07:54:57 +0900

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,24 +1,39 @@
 import * as React from "react";
-import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import Layout from "../components/layout";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>Last modified: {node.parent.modifiedTime}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "YYYY년 MM월 DD일")
+          title
+        }
+        id
+        parent {
+          ... on File {
+            name
+            modifiedTime(formatString: "YYYY년 MM월 DD일, hh시 mm분")
+          }
+        }
+        body
       }
     }
   }
